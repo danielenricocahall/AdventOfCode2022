a555fb435447d6a191a72c026f3cf43755a2faab
danielenricocahall <danielenricocahall@gmail.com>
2022-12-03 09:58:02 -0500

    part 2 done

--- Day2/solution.py
+++ Day2/solution.py
@@ -23,6 +23,29 @@ class GameOption(str, Enum):
         elif other == GameOption.SCISSORS:
             return self == GameOption.PAPER
 
+    def __gt__(self, other):
+        if other == GameOption.ROCK:
+            return self == GameOption.PAPER
+        elif other == GameOption.PAPER:
+            return self == GameOption.SCISSORS
+        elif other == GameOption.SCISSORS:
+            return self == GameOption.ROCK
+
+
+class GameOutcome(str, Enum):
+    WIN: str = "win"
+    LOSE: str = "lose"
+    DRAW: str = "draw"
+
+    def __str__(self):
+        return self.value
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
 
 if __name__ == "__main__":
     rock_paper_scissors_mapping = {
@@ -33,6 +56,13 @@ if __name__ == "__main__":
         'Y': GameOption.PAPER,
         'Z': GameOption.SCISSORS
     }
+
+    outcome_mapping = {
+        'X': GameOutcome.LOSE,
+        'Y': GameOutcome.DRAW,
+        'Z': GameOutcome.WIN
+    }
+
     WINNING_POINTS = 6
     DRAW_POINTS = 3
     point_mapping = {
@@ -44,14 +74,18 @@ if __name__ == "__main__":
         games = f.read().splitlines()
         total_points = 0
         for game in games:
-            opponent_move, my_move = map(rock_paper_scissors_mapping.get, game.split(' '))
+            opponent_move, outcome = game.split(' ')
+            opponent_move = rock_paper_scissors_mapping[opponent_move]
+            outcome = outcome_mapping[outcome]
+            if outcome == GameOutcome.WIN:
+                my_move = next((move for move in GameOption if move > opponent_move))
+            elif outcome == GameOutcome.LOSE:
+                my_move = next((move for move in GameOption if move < opponent_move))
+            else:
+                my_move = opponent_move
             total_points += point_mapping[my_move]
             if opponent_move == my_move:
                 total_points += DRAW_POINTS
             elif opponent_move < my_move:
                 total_points += WINNING_POINTS
         print(total_points)
-
-
-
-
